9e368e92a28327194f0538e4f230e6cfc580dd1d
Hadenir <hadenir@gmail.com>
2022-05-02 16:34:34 +0200

    Proper json parsing and printing

--- json-attoparsec/src/Json.hs
+++ json-attoparsec/src/Json.hs
@@ -1,7 +1,9 @@
-module Json where
+module Json
+    ( Value (..)
+    ) where
 
 import qualified Data.Map.Strict as M
-import Data.Text
+import Data.Text (Text)
 
 data Value = Null
     | Boolean Bool
@@ -9,4 +11,30 @@ data Value = Null
     | Number Double
     | Array [Value]
     | Object (M.Map Text Value)
-    deriving (Show)
+
+instance Show Value where
+    show = showJson 0
+
+prefix :: Int -> String
+prefix depth = replicate (depth * 4) ' '
+
+showJson :: Int -> Value -> String
+showJson _ Null = "null"
+showJson _ (Boolean b) = show b
+showJson _ (String text) = show text
+showJson _ (Number num) = show num
+showJson _ (Array []) = "[]"
+showJson depth (Array arr) = "[\n" ++ showJsonArray arr ++ prefix depth ++ "]"
+    where
+        depth1 = depth + 1
+        showJsonArray [] = ""
+        showJsonArray [v] = prefix depth1 ++ showJson depth1 v ++ "\n"
+        showJsonArray (v:vs) = prefix depth1 ++ showJson depth1 v ++ ",\n" ++ showJsonArray vs
+showJson depth (Object members)
+    | M.null members = "{}"
+    | otherwise = "{\n" ++ showJsonMembers (M.assocs members) ++ prefix depth  ++ "}"
+    where
+        depth1 = depth + 1
+        showJsonMembers [] = ""
+        showJsonMembers [(k,v)] = prefix depth1 ++ show k ++ ": " ++ showJson depth1 v ++ "\n"
+        showJsonMembers ((k,v):ps) = prefix depth1 ++ show k ++ ": " ++ showJson depth1 v ++ ",\n" ++ showJsonMembers ps
--- json-attoparsec/src/Lib.hs
+++ json-attoparsec/src/Lib.hs
@@ -2,8 +2,13 @@ module Lib
     ( someFunc
     ) where
 
-import Data.Attoparsec.Text as P
+import qualified Data.Attoparsec.Text.Lazy as P
+import qualified Data.Text.Lazy.IO as L
 import JsonParser
 
 someFunc :: IO ()
-someFunc = print (P.parseOnly stringParser "\"aasasd\"")
+someFunc = do
+    text <- L.readFile "large-file.json"
+    case P.parse jsonParser text of
+        P.Fail {} -> putStrLn "Failed"
+        P.Done _ json -> putStrLn ("Succeded:\n" ++ show json)
