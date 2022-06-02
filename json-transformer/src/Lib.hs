dfb17cd65e375a71ca81146fa93183093d9854b0
Hadenir <hadenir@gmail.com>
2022-06-02 14:32:02 +0200

    Better output formatting

--- json-transformer/src/Lib.hs
+++ json-transformer/src/Lib.hs
@@ -14,29 +14,31 @@ import JsonParser
 import JsonTransformer
 
 someFunc :: IO ()
-someFunc = runEffect $ parseJsonFile "testfile.json" >-> transform >-> prettyPrint
+someFunc = runEffect $ parseJsonFile "largetest.json" >-> transform >-> prettyPrint
 
 prettyPrint :: Consumer Json.Token IO ()
-prettyPrint = flip evalStateT 0 $ forever $ do
+prettyPrint = flip evalStateT (0, False, False) $ forever $ do
     token <- lift await
     case token of
-        Json.ArrayEnd -> modify $ subtract 1
-        Json.ObjectEnd -> modify $ subtract 1
+        Json.ArrayEnd -> modify $ \(d,_,_) -> (d-1,False,True)
+        Json.ObjectEnd -> modify $ \(d,_,_) -> (d-1,False,True)
         _ -> return ()
-    depth <- get
+    (depth, comma, newline) <- get
     lift $ lift $ do
-        putStr $ concat $ replicate depth "  "
+        putStr $ if comma then  "," else ""
+        putStr $ if newline then "\n" ++ concat (replicate depth "  ") else " "
         case token of
-            Json.Null -> putStrLn "null"
-            Json.Boolean bool ->  print bool
-            Json.String text -> T.putStrLn $ T.concat ["\"", text, "\""]
-            Json.Number num -> print num
-            Json.ArrayBegin -> putStrLn "["
-            Json.ArrayEnd -> putStrLn "]"
-            Json.ObjectBegin -> putStrLn "{"
-            Json.ObjectEnd -> putStrLn "}"
-            Json.ObjectField key -> T.putStrLn $ T.concat ["\"", key, "\":"]
+            Json.Null -> putStr "null"
+            Json.Boolean bool ->  putStr $ show bool
+            Json.String text -> T.putStr $ T.concat ["\"", text, "\""]
+            Json.Number num -> putStr $ show num
+            Json.ArrayBegin -> putStr "["
+            Json.ArrayEnd -> putStr "]"
+            Json.ObjectBegin -> putStr "{"
+            Json.ObjectEnd -> putStr "}"
+            Json.ObjectField key -> T.putStr $ T.concat ["\"", key, "\":"]
     case token of
-        Json.ArrayBegin -> modify (+1)
-        Json.ObjectBegin -> modify (+1)
-        _ -> return ()
+        Json.ArrayBegin -> modify $ \(d,_,_) -> (d+1,False,True)
+        Json.ObjectBegin -> modify $ \(d,_,_) -> (d+1,False,True)
+        Json.ObjectField _ -> modify $ \(d,_,_) -> (d,False,False)
+        _ -> modify $ \(d,_,_) -> (d,True,True)
